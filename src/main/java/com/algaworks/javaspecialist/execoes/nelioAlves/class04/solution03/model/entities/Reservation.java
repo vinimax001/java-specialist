9326bc724ea1f3f3dcb7326b1ee278c10100860b
vinimax <vini.ciusang@gmail.com>
2026-05-02 20:49:17 -0300

    feat: add class04(God solution)

---
+++ src/main/java/com/algaworks/javaspecialist/execoes/nelioAlves/class04/solution03/model/entities/Reservation.java
@@ -0,0 +1,67 @@
+package com.algaworks.javaspecialist.execoes.nelioAlves.class04.solution03.model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class  Reservation {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public long duration(){
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkOut) {
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            throw new IllegalArgumentException ("Reservation dates for update must be future dates.") ;
+        }
+        if (!checkOut.after(checkIn)) {
+            throw new IllegalArgumentException ("Check-out data must be after check-in date.");
+        }
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString(){
+        return "Room "
+                + roomNumber
+                + ", check-in: "
+                + sdf.format(checkIn)
+                + ", "
+                + "check-out: "
+                + sdf.format(checkOut)
+                + ", "
+                + duration()
+                + " nights";
+    }
+
+    //GETTERS E SETTERS
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+}
