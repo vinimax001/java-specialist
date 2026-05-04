e06b323ad8ba8669bc15f8a2c054ef5f52e59288
vinimax <vini.ciusang@gmail.com>
2026-05-04 20:59:02 -0300

    feat add class04(Correct solution: custom exceptions)

---
+++ src/main/java/com/algaworks/javaspecialist/execoes/nelioAlves/class04/solution04final/model/exceptions/DomainException.java
@@ -0,0 +1,9 @@
+package com.algaworks.javaspecialist.execoes.nelioAlves.class04.solution04final.model.exceptions;
+
+public class DomainException extends Exception{
+    private static final long serialVersionUID = 1L;
+
+    public DomainException(String msg) {
+        super(msg);
+    }
+}
